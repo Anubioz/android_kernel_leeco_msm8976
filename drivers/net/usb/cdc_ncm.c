7a7aa1c2bfde3bce747c9f6c87378542cfc0c2d0
Anubioz <anubioz.github.com@zeracles.net.ru>
2019-04-26 17:54:29 +0300

    ResurrectionRemix 7.0.2 Kernel Source code

--- drivers/net/usb/cdc_ncm.c
+++ drivers/net/usb/cdc_ncm.c
@@ -1158,12 +1158,8 @@ static void cdc_ncm_disconnect(struct usb_interface *intf)
 
 static const struct driver_info cdc_ncm_info = {
 	.description = "CDC NCM",
-	.flags = FLAG_POINTTOPOINT | FLAG_NO_SETINT | FLAG_MULTI_PACKET
-<<<<<<< HEAD
+	.flags = FLAG_POINTTOPOINT | FLAG_NO_SETINT | FLAG_MULTI_PACKET 
                         | FLAG_LINK_INTR,
-=======
-			| FLAG_LINK_INTR,
->>>>>>> v3.10.103
 	.bind = cdc_ncm_bind,
 	.unbind = cdc_ncm_unbind,
 	.check_connect = cdc_ncm_check_connect,
